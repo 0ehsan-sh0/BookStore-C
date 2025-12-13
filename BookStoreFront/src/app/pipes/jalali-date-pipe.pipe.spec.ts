22be2d117c50d25b7a58a330ceef26d1ef1c0cfb
ehsan <ehsan.sh400@gmail.com>
2025-12-13 21:56:01 +0330

    all spec files are deleted.

--- BookStoreFront/src/app/pipes/jalali-date-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { JalaliDatePipePipe } from './jalali-date.pipe';
-
-describe('JalaliDatePipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new JalaliDatePipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
